fe35885c6fcf246e6819b0c2ff2258306ca0218f
JunimCria <gilbertojuniorcc@gmail.com>
2023-10-23 22:48:32 -0300

    feet: Logando e gerando Token JWT

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,5 +1,7 @@
 import prismaClient from "../../prisma";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
 interface AuthRequest {
   email: string;
   password: string;
@@ -24,7 +26,26 @@ class AuthUserService {
       throw new Error("User/password incorrect");
     }
 
-    return { ok: true };
+    //Se deu tudo certo , gerar o token para o usuario
+
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "30d",
+      }
+    );
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token,
+    };
   }
 }
 
